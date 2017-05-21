81b4cbb4629b7db1d4ef750bbdb7ebf951f4f1c7
Oleksandr Slynko <oslynko@pivotal.io>
2017-05-21 22:09:11 +0100

    Shellcheck + stop create links to required files

--- install.sh
+++ install.sh
@@ -6,14 +6,14 @@ for name in *; do
   target="$HOME/.$name"
   if [ -e "$target" ]; then
     if [ ! -L "$target" ]; then
-      cutline=`grep -n -m1 "$cutstring" "$target" | sed "s/:.*//"`
+      cutline=$(grep -n -m1 "$cutstring" "$target" | sed "s/:.*//")
       if [ -n "$cutline" ]; then
-	cutline=$((cutline-1))
+        cutline=$((cutline-1))
         echo "Updating $target"
         head -n $cutline "$target" > update_tmp
-        startline=`sed '1!G;h;$!d' "$name" | grep -n -m1 "$cutstring" | sed "s/:.*//"`
+        startline=$(sed '1!G;h;$!d' "$name" | grep -n -m1 "$cutstring" | sed "s/:.*//")
         if [ -n "$startline" ]; then
-          tail -n $startline "$name" >> update_tmp
+          tail -n "$startline" "$name" >> update_tmp
         else
           cat "$name" >> update_tmp
         fi
@@ -23,9 +23,9 @@ for name in *; do
       fi
     fi
   else
-    if [ "$name" != 'install.sh' ]; then
+    if [ "$name" != 'install.sh' ] && [ "$name" != 'install.cmd' ] && [ "$name" != 'Brewfile' ]; then
       echo "Creating $target"
-      if [ -n "$(grep "$cutstring" "$name")" ]; then
+      if grep -q "$cutstring" "$name"; then
         cp "$PWD/$name" "$target"
       else
         ln -s "$PWD/$name" "$target"
